a04a0c37012aa22a5cd442a86770f733b0aea924
Chow Jia Ying <chowjiaying211@gmail.com>
2021-07-26 00:04:05 +0800

    feat: allow adding tags using a taginput

--- src/SearchBar/index.tsx
+++ src/SearchBar/index.tsx
@@ -3,15 +3,24 @@ import React from 'react';
 import ToolList from "src/ToolList";
 import FilterTag from "src/Tags/FilterTags";
 
+import './index.css';
+
 // TODO: create a tag input component
 const NavBar: React.FC = () => {
-  const [tags, setTags] = React.useState<string[]>([]);
+  const [tags, setTags] = React.useState<string[]>(["Frontend"]);
+  const handleAddTags = (event) => {
+    if (event.code === "Enter") {
+      const { target: { value } } = event;
+      setTags([...tags, value]);
+      event.target.value = "";
+    }
+  }
   return (
       <div className="container">
       <ToolList>
       {tags.map(tag => <FilterTag key={tag} isCancelable>{tag}</FilterTag>)}
       </ToolList>
-      <input />
+      <input onKeyPress={handleAddTags}className="search" />
       </div>
       );
 }
