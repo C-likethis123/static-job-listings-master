898e8bc5d6da00596cfe42a922f3c24e89e0d561
Chow Jia Ying <chowjiaying211@gmail.com>
2021-07-28 23:20:51 +0800

    feat: allow users to filter via tags

--- src/JobList.tsx
+++ src/JobList.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
+/** Contexts */
+import {FilterContext} from "src/FilterContext";
+
+/** Data */
 import data from "src/data.json";
+
+/** Components */
 import Job from "./Job";
+
 const JobList: React.FC = () => {
-  return (<div>
-    {data.map(({id, ...rest}) => <Job key={id} {...rest} />)}
-  </div>);
+  const {tags} = useContext(FilterContext);
+  const filteredData = data.filter(({languages, tools, role, level}) => {
+    const allInformation = [...languages, ...tools, role, level];
+    return tags.length === 0 || allInformation.some((value) => tags.includes(value));
+  });
+
+  return (
+    <>
+      {filteredData.map(({id, ...rest}) => <Job key={id} {...rest} />)}
+    </>
+  );
 };
 
 export default JobList;
