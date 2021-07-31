3f6b2ac8fa3552337370344164072d6b9f1e66c9
Chow Jia Ying <chowjiaying211@gmail.com>
2021-07-31 15:51:39 +0800

    feat: add settings for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '<static-job-listings-master>',
   plugins: [
     reactRefresh(),
     tsconfigPaths(),
